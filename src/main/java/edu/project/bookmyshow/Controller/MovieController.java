3ccc50d2883b5bd9c1ba5360f674159836942438
unknown <poojithadarshan2131@gmail.com>
2023-05-06 12:14:41 +0530

    added swagger documentation

--- src/main/java/edu/project/bookmyshow/Controller/MovieController.java
+++ src/main/java/edu/project/bookmyshow/Controller/MovieController.java
@@ -15,25 +15,43 @@ import edu.project.bookmyshow.dto.MovieDto;
 import edu.project.bookmyshow.entity.Movie;
 import edu.project.bookmyshow.service.MovieService;
 import edu.project.bookmyshow.util.ResponseStructure;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
 	@Autowired
 	private MovieService service;
-	
+
+	@ApiOperation(value = "Save Movie", notes = " Api is used to save the Movie")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully created"),
+			@ApiResponse(code = 404, message = "Movie not found for the given  id") })
+
 	@PostMapping
 	public ResponseEntity<ResponseStructure<Movie>> saveMovie(@RequestParam long houseId,@RequestBody MovieDto movieDto){
 		return service.saveMovie(houseId,movieDto);
 	}
+
+	@ApiOperation(value = "Find Movie", notes = " Api is used to find the Movie")
+	@ApiResponses(value = { @ApiResponse(code = 302, message = "Successfully fetched"),
+			@ApiResponse(code = 404, message = "Movie not found for the given  id") })
     @GetMapping
     public ResponseEntity<ResponseStructure<Movie>> getMovie(@RequestParam long movieId){
     	return service.getMovieById(movieId);
     }
+	@ApiOperation(value = "Delete Movie", notes = " Api is used to delete the Movie")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully deleted"),
+			@ApiResponse(code = 404, message = "Movie not found for the given  id") })
     @DeleteMapping
     public ResponseEntity<ResponseStructure<Movie>> deleteMovie(@RequestParam long movieId){
     	return service.deleteMovieById(movieId);
     }
+
+	@ApiOperation(value = "Update Movie", notes = " Api is used to update the Movie")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully Updated"),
+			@ApiResponse(code = 404, message = "Movie not found for the given  id") })
     @PutMapping
     public ResponseEntity<ResponseStructure<Movie>> updateMovie(@RequestParam long movieId,@RequestBody MovieDto movieDto){
     	return service.updateMovie(movieId, movieDto);
--- src/main/java/edu/project/bookmyshow/Controller/TheaterController.java
+++ src/main/java/edu/project/bookmyshow/Controller/TheaterController.java
@@ -14,25 +14,35 @@ import edu.project.bookmyshow.dto.TheatreDto;
 import edu.project.bookmyshow.entity.Theatre;
 import edu.project.bookmyshow.service.TheaterService;
 import edu.project.bookmyshow.util.ResponseStructure;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/theater")
 public class TheaterController {
 	@Autowired
 	private TheaterService service;
-
+	@ApiOperation(value = "Save Theatre", notes = " Api is used to save the Theatre")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully created"),
+			@ApiResponse(code = 404, message = "Theatre not found for the given  id") })
 	@PostMapping
 	public ResponseEntity<ResponseStructure<Theatre>> addTheatre(@RequestParam long ownerId,
 			@RequestBody TheatreDto dto) {
 		return service.addTheatre(ownerId, dto);
 	}
-
+	@ApiOperation(value = "Update Theatre", notes = " Api is used to update the Theatre")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully Updated"),
+			@ApiResponse(code = 404, message = "Theatre not found for the given  id") })
 	@PutMapping
 	public ResponseEntity<ResponseStructure<Theatre>> updateTheatre(@RequestParam long theaterId,
 			@RequestBody TheatreDto dto) {
 		return service.updateTheatre(theaterId, dto);
 	}
 
+	@ApiOperation(value = "Delete Theatre", notes = " Api is used to delete the Theatre")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully deleted"),
+			@ApiResponse(code = 404, message = "Theatre not found for the given  id") })
 	@DeleteMapping
 	public ResponseEntity<ResponseStructure<Theatre>> deleteTheatre(@RequestParam long theaterId) {
 		return service.deleteTheatre(theaterId);
