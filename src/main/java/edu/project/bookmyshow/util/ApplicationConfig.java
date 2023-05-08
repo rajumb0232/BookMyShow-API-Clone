1f12bf7ee68348f3056fc66bbb3033ebc9300dd0
Sharathc18 <122531284+Sharathc18@users.noreply.github.com>
2023-05-08 14:43:50 +0530

    final push

--- src/main/java/edu/project/bookmyshow/util/ApplicationConfig.java
+++ src/main/java/edu/project/bookmyshow/util/ApplicationConfig.java
@@ -19,27 +19,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ApplicationConfig {
 
 	@Bean
-	
-		@SuppressWarnings("rawtypes")
-		public Docket getDocket() {
-			Contact contact = new Contact("TYSS Team", null, "rajugowda0212@gmail.com");
-			List<VendorExtension> extensions = new ArrayList<>();
-			ApiInfo apiInfo = new ApiInfo("Shopping App - Angadi API",
-					
-					"BookMyShow-Clone-API is a project for online Ticket-Booking-System. The \r\n"
-							+ "BookMyShow clone API will be used to book Movie tickets online, the application \r\n"
-							+ "will allow users to select the location and then search for the movies based on that \r\n"
-							+ "location, the user can select the Screen and number of seats they want, the \r\n"
-							+ "application will help eliminating the need of physical ticket counters, this \r\n"
-							+ "application will also auto update the show status, and maintains all the show \r\n"
-							+ "history. \r\n"
-							+ "",
-					
-					"1.0 first", "", contact, "", "", extensions);
-			
-			return new Docket(DocumentationType.SWAGGER_2).select()
-					.apis(RequestHandlerSelectors.basePackage("edu.project.bookmyshow")).build()
-					.apiInfo(apiInfo).useDefaultResponseMessages(false);
-		
+	@SuppressWarnings("rawtypes")
+	public Docket getDocket() {
+		Contact contact = new Contact("TYSS Team", null, "rajugowda0212@gmail.com");
+		List<VendorExtension> extensions = new ArrayList<>();
+		ApiInfo apiInfo = new ApiInfo("BookMyShow-Clone-API",
+
+				"BookMyShow-Clone-API is a project for online Ticket-Booking-System. The \r\n"
+						+ "BookMyShow clone API will be used to book Movie tickets online, the application \r\n"
+						+ "will allow users to select the location and then search for the movies based on that \r\n"
+						+ "location, the user can select the Screen and number of seats they want, the \r\n"
+						+ "application will help eliminating the need of physical ticket counters, this \r\n"
+						+ "application will also auto update the show status, and maintains all the show \r\n"
+						+ "history. \r\n" + "",
+
+				"1.0 first", "", contact, "", "", extensions);
+
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("edu.project.bookmyshow")).build().apiInfo(apiInfo)
+				.useDefaultResponseMessages(false);
+
 	}
 }
